c5141948c176fa614c9e2de9b74f84a8c5bac15d
DarkerSeven <alconnor@yandex.ru>
2026-09-03 21:23:21 +0300

    Add point to screen

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -13,8 +13,19 @@ namespace Tetris
            
             Console.SetWindowSize(40,30);
             Console.SetBufferSize(40,30);
-         
+            
+            int x1 = 20;
+            int y1 = 15;
+            char c1 = '*';
+            draw(x1, y1, c1);
+            
             Console.ReadLine();
+        }
+
+        private static void draw(int x1, int y1, char c1)
+        {
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(c1);
         }
     }
 }
