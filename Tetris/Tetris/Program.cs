c7f5fed9d24e7b16776b4d40c6f646c1bf48b75f
DarkerSeven <alconnor@yandex.ru>
2026-09-03 22:02:49 +0300

    Add class Point

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -14,18 +14,16 @@ namespace Tetris
             Console.SetWindowSize(40,30);
             Console.SetBufferSize(40,30);
             
-            int x1 = 20;
-            int y1 = 15;
-            char c1 = '*';
-            draw(x1, y1, c1);
+            Point point = new Point();
+
+            point.x = 20;
+            point.y = 15;
+            point.c = '*';
+            point.Draw();
             
             Console.ReadLine();
         }
 
-        private static void draw(int x1, int y1, char c1)
-        {
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(c1);
-        }
+       
     }
 }
