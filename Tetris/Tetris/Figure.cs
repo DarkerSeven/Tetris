4b194aef6a51ac79b9f49aac4d02b17afc1f9c68
DarkerSeven <alconnor@yandex.ru>
2026-09-04 19:48:13 +0300

    Update

---
+++ Tetris/Tetris/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Figure
+    {
+        protected Point[] points = new Point[4];
+        public Figure()
+        {
+            
+        }
+        public void Draw()
+        {
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
